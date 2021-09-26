f522d47e7ef4c115519141bef79b0c4ef73e6839
Michael Tedeschi <mike.tedeschi@gmail.com>
2021-09-26 12:13:05 -0400

    Add headers to modals

--- src/components/Collaborators/CollaboratorsModal/CollaboratorsModal.js
+++ src/components/Collaborators/CollaboratorsModal/CollaboratorsModal.js
@@ -9,6 +9,10 @@ export function CollaboratorsModal(props) {
   
   return (
 	<div className={ visiibility }>
+	  <div className="modal--header">
+			<h1>Create New Workshop</h1>
+			<button className="modal--close"><i className="bi-x-circle"></i></button>
+	  </div>
 	  <div className="modal--container">
 	  	<h1>Manage Collaborators</h1>
 		<div className="button-group">
--- src/components/Dashboard/CreateNewModal/CreateNewModal.js
+++ src/components/Dashboard/CreateNewModal/CreateNewModal.js
@@ -70,16 +70,19 @@ export function CreateNewModal(props) {
   
   return (
 	<div className={ visiibility }>
+		<div className="modal--header">
+			<h1>Create New Workshop</h1>
+			<button className="modal--close"><i className="bi-x-circle"></i></button>
+	  </div>
 	  <div className="modal--container">
-	  	<h1>Create New Workshop</h1>
 	  	<fieldset>
 	  		<label for="name">Workshop Name</label>
 	  		<input id="name" type="text" value={name} onChange={handleUpdateName} />
 	  	</fieldset>
-		<div className="button-group">
-			<a className="btn btn-primary" onClick={insertWorkshop}>Create</a>
-			<a className="btn btn-outline" onClick={closeModal}>Cancel</a>
-		</div>
+			<div className="button-group">
+				<a className="btn btn-primary" onClick={insertWorkshop}>Create</a>
+				<a className="btn btn-outline" onClick={closeModal}>Cancel</a>
+			</div>
 	  </div>
 	</div>
   );
